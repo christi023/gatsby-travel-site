d20d3d1abc84e9943b00b82dc9635ba9365ec51c
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-01 19:07:24 +0200

    Navbar with JS Styles

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from "react"
 import { FaBars, FaTimes } from "react-icons/fa"
 import { IconContext } from "react-icons/lib"
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements"
 
 export default function Navbar() {
   // states
   const [click, setClick] = useState(false)
   const [scroll, setScroll] = useState(false)
+
+  //handleClick toggle mobile icon
+  const handleClick = () => setClick(!click)
 
   // change navbar function
   const changeNav = () => {
@@ -31,6 +44,20 @@ export default function Navbar() {
               <NavIcon />
               Destinations
             </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/images">Images</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/destinations">Destinations</NavLinks>
+              </NavItem>
+            </NavMenu>
           </NavbarContainer>
         </Nav>
       </IconContext.Provider>
