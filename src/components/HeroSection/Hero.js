f7aba2f7866aba4a2bc630e0c17daefb1ccb94a0
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-04 22:01:46 +0200

    Hero Section created along with Hero css

---
+++ src/components/HeroSection/Hero.js
@@ -0,0 +1,14 @@
+import React from "react"
+// styles
+import "./Hero.css"
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <h1>Travel More</h1>
+      <p>Plan your next trip today</p>
+      <div className="hero-btns">
+        <button>Button</button>
+      </div>
+    </div>
+  )
+}
