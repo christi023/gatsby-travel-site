89812df84a87ef4abbb5341028fe5246cf717296
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-04 22:07:14 +0200

    added Hero Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 //import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-
 import SEO from "../components/seo"
+import HeroSection from "../components/HeroSection/Hero"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home Page</h1>
+    <HeroSection />
   </Layout>
 )
 
