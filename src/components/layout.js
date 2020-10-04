38e1e5badb0a05ca7ac31d72aa4ce68a3c528bfc
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-04 22:04:40 +0200

    layout for website includes Navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Navbar from "./Navbar/Navbar"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <Navbar />
